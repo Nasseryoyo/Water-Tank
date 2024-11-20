8afe4b8238f462dc75baa28b5fe67d769083c141
DottierDark <ashmxwy@gmail.com>
2024-11-20 21:37:36 +0200

    testing LCD

--- MCU2/Drivers/Actuators/LCD/LCD.c
+++ MCU2/Drivers/Actuators/LCD/LCD.c
@@ -1,26 +1,32 @@
 #include <Drivers/Actuators/LCD/LCD.h>
 
-uint32_t pin_values_to_mask(LCDdisplay* lcd, uint32_t raw_bits[], int length) {
+uint32_t pin_values_to_mask(LCDdisplay *lcd, uint32_t raw_bits[], int length)
+{
     uint32_t result = 0;
     uint32_t pinArray[32] = {0};
-    for (int i = 0; i < length; i++) {
+    for (int i = 0; i < length; i++)
+    {
         pinArray[lcd->LCDpins[i]] = raw_bits[i];
     }
-    for (int i = 0; i < 32; i++) {
+    for (int i = 0; i < 32; i++)
+    {
         result = result << 1;
-        result += pinArray[31-i];
+        result += pinArray[31 - i];
     }
     return result;
 }
 
-void uint_into_8bits(uint32_t raw_bits[], uint32_t one_byte) {
-    for (int i = 0; i < 8; i++) {
-        raw_bits[7-i] = one_byte % 2;
+void uint_into_8bits(uint32_t raw_bits[], uint32_t one_byte)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        raw_bits[7 - i] = one_byte % 2;
         one_byte = one_byte >> 1;
     }
 }
 
-void init_pwm_pin(LCDdisplay* lcd, uint32_t pin) {
+void init_pwm_pin(LCDdisplay *lcd, uint32_t pin)
+{
     lcd->bl_pwm_pin = pin;
     gpio_set_function(pin, GPIO_FUNC_PWM);
     uint slice_num = pwm_gpio_to_slice_num(pin);
@@ -30,7 +36,8 @@ void init_pwm_pin(LCDdisplay* lcd, uint32_t pin) {
     pwm_init(slice_num, &config, true);
 }
 
-void send_raw_data_one_cycle(LCDdisplay* lcd, uint32_t raw_bits[]) {
+void send_raw_data_one_cycle(LCDdisplay *lcd, uint32_t raw_bits[])
+{
     uint32_t bit_value = pin_values_to_mask(lcd, raw_bits, 5);
     gpio_put_masked(lcd->LCDmask, bit_value);
     gpio_put(lcd->LCDpins[E], HIGH);
@@ -39,20 +46,24 @@ void send_raw_data_one_cycle(LCDdisplay* lcd, uint32_t raw_bits[]) {
     sleep_ms(5);
 }
 
-void send_full_byte(LCDdisplay* lcd, uint32_t rs, uint32_t databits[]) {
+void send_full_byte(LCDdisplay *lcd, uint32_t rs, uint32_t databits[])
+{
     uint32_t rawbits[5];
     rawbits[4] = rs;
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 4; i++)
+    {
         rawbits[i] = databits[i];
     }
     send_raw_data_one_cycle(lcd, rawbits);
-    for (int i = 0; i < 4; i++) {
-        rawbits[i] = databits[i+4];
+    for (int i = 0; i < 4; i++)
+    {
+        rawbits[i] = databits[i + 4];
     }
     send_raw_data_one_cycle(lcd, rawbits);
 }
 
-void LCDdisplay_init(LCDdisplay* lcd, int bit4_pin, int bit5_pin, int bit6_pin, int bit7_pin, int rs_pin, int e_pin, int width, int depth) {
+void LCDdisplay_init(LCDdisplay *lcd, int bit4_pin, int bit5_pin, int bit6_pin, int bit7_pin, int rs_pin, int e_pin, int width, int depth)
+{
     lcd->LCDpins[0] = bit7_pin;
     lcd->LCDpins[1] = bit6_pin;
     lcd->LCDpins[2] = bit5_pin;
@@ -64,66 +75,78 @@ void LCDdisplay_init(LCDdisplay* lcd, int bit4_pin, int bit5_pin, int bit6_pin, 
     lcd->no_lines = depth;
 }
 
-void LCDdisplay_init_with_bl(LCDdisplay* lcd, int bit4_pin, int bit5_pin, int bit6_pin, int bit7_pin, int rs_pin, int e_pin, int bl_pin, int width, int depth) {
+void LCDdisplay_init_with_bl(LCDdisplay *lcd, int bit4_pin, int bit5_pin, int bit6_pin, int bit7_pin, int rs_pin, int e_pin, int bl_pin, int width, int depth)
+{
     LCDdisplay_init(lcd, bit4_pin, bit5_pin, bit6_pin, bit7_pin, rs_pin, e_pin, width, depth);
     lcd->bl_pwm_pin = bl_pin;
+    LCDdisplay_init_display(lcd); // Ensure the display is initialized
 }
 
-void LCDdisplay_clear(LCDdisplay* lcd) {
-    uint32_t clear_display[8] = {0,0,0,0,0,0,0,1};
+void LCDdisplay_clear(LCDdisplay *lcd)
+{
+    uint32_t clear_display[8] = {0, 0, 0, 0, 0, 0, 0, 1};
     send_full_byte(lcd, COMMAND, clear_display);
     sleep_ms(10);
 }
 
-void LCDdisplay_cursor_off(LCDdisplay* lcd) {
-    uint32_t no_cursor[8] = {0,0,0,0,1,1,0,0};
+void LCDdisplay_cursor_off(LCDdisplay *lcd)
+{
+    uint32_t no_cursor[8] = {0, 0, 0, 0, 1, 1, 0, 0};
     send_full_byte(lcd, COMMAND, no_cursor);
     lcd->cursor_status[0] = false;
     lcd->cursor_status[1] = false;
 }
 
-void LCDdisplay_cursor_on(LCDdisplay* lcd) {
-    uint32_t command_cursor[8] = {0,0,0,0,1,1,1,1};
+void LCDdisplay_cursor_on(LCDdisplay *lcd)
+{
+    uint32_t command_cursor[8] = {0, 0, 0, 0, 1, 1, 1, 1};
     send_full_byte(lcd, COMMAND, command_cursor);
     lcd->cursor_status[0] = true;
     lcd->cursor_status[1] = true;
 }
 
-void LCDdisplay_cursor_on_blink(LCDdisplay* lcd, bool blink) {
-    uint32_t command_cursor[8] = {0,0,0,0,1,1,1,0};
-    if (blink) command_cursor[7] = 1;
+void LCDdisplay_cursor_on_blink(LCDdisplay *lcd, bool blink)
+{
+    uint32_t command_cursor[8] = {0, 0, 0, 0, 1, 1, 1, 0};
+    if (blink)
+        command_cursor[7] = 1;
     send_full_byte(lcd, COMMAND, command_cursor);
     lcd->cursor_status[0] = true;
     lcd->cursor_status[1] = command_cursor[7];
 }
 
-void LCDdisplay_display_off(LCDdisplay* lcd) {
-    uint32_t command_display[8] = {0,0,0,0,1,0,0,0};
+void LCDdisplay_display_off(LCDdisplay *lcd)
+{
+    uint32_t command_display[8] = {0, 0, 0, 0, 1, 0, 0, 0};
     command_display[7] = lcd->cursor_status[1];
     command_display[6] = lcd->cursor_status[0];
     send_full_byte(lcd, COMMAND, command_display);
 }
 
-void LCDdisplay_display_on(LCDdisplay* lcd) {
-    uint32_t command_display[8] = {0,0,0,0,1,1,0,0};
+void LCDdisplay_display_on(LCDdisplay *lcd)
+{
+    uint32_t command_display[8] = {0, 0, 0, 0, 1, 1, 0, 0};
     command_display[7] = lcd->cursor_status[1];
     command_display[6] = lcd->cursor_status[0];
     send_full_byte(lcd, COMMAND, command_display);
 }
 
-void LCDdisplay_set_backlight(LCDdisplay* lcd, int brightness) {
-    if (lcd->bl_pwm_pin < 30) {
+void LCDdisplay_set_backlight(LCDdisplay *lcd, int brightness)
+{
+    if (lcd->bl_pwm_pin < 30)
+    {
         pwm_set_gpio_level(lcd->bl_pwm_pin, brightness);
     }
 }
 
-void LCDdisplay_init_display(LCDdisplay* lcd) {
-    uint32_t all_ones[6] = {1,1,1,1,1,1};
-    uint32_t set_function_8[5] = {0,0,1,1,0};
-    uint32_t set_function_4a[5] = {0,0,1,0,0};
-    uint32_t set_function_4[8] = {0,0,1,0,0,0,0,0};
-    uint32_t cursor_set[8] = {0,0,0,0,0,1,1,0};
-    uint32_t display_prop_set[8] = {0,0,0,0,1,1,0,0};
+void LCDdisplay_init_display(LCDdisplay *lcd)
+{
+    uint32_t all_ones[6] = {1, 1, 1, 1, 1, 1};
+    uint32_t set_function_8[5] = {0, 0, 1, 1, 0};
+    uint32_t set_function_4a[5] = {0, 0, 1, 0, 0};
+    uint32_t set_function_4[8] = {0, 0, 1, 0, 0, 0, 0, 0};
+    uint32_t cursor_set[8] = {0, 0, 0, 0, 0, 1, 1, 0};
+    uint32_t display_prop_set[8] = {0, 0, 0, 0, 1, 1, 0, 0};
 
     lcd->LCDmask_c = pin_values_to_mask(lcd, all_ones, 6);
     lcd->LCDmask = pin_values_to_mask(lcd, all_ones, 5);
@@ -131,7 +154,8 @@ void LCDdisplay_init_display(LCDdisplay* lcd) {
     gpio_set_dir_out_masked(lcd->LCDmask_c);
     gpio_clr_mask(lcd->LCDmask_c);
 
-    if (lcd->no_lines == 2) {
+    if (lcd->no_lines == 2)
+    {
         set_function_4[4] = 1;
     }
     send_raw_data_one_cycle(lcd, set_function_8);
@@ -144,7 +168,8 @@ void LCDdisplay_init_display(LCDdisplay* lcd) {
     send_full_byte(lcd, COMMAND, display_prop_set);
     LCDdisplay_clear(lcd);
 
-    if (lcd->bl_pwm_pin < 30) {
+    if (lcd->bl_pwm_pin < 30)
+    {
         init_pwm_pin(lcd, lcd->bl_pwm_pin);
     }
 
@@ -152,50 +177,60 @@ void LCDdisplay_init_display(LCDdisplay* lcd) {
     lcd->cursor_status[1] = false;
 }
 
-void LCDdisplay_goto_pos(LCDdisplay* lcd, int pos_i, int line) {
+void LCDdisplay_goto_pos(LCDdisplay *lcd, int pos_i, int line)
+{
     uint32_t eight_bits[8];
     uint32_t pos = (uint32_t)pos_i;
-    switch (lcd->no_lines) {
-        case 2:
-            pos = 64*line + pos + 0b10000000;
-            break;
-        case 4:
-            if (line == 0 || line == 2) {
-                pos = 64*(line/2) + pos + 0b10000000;
-            } else {
-                pos = 64*((line-1)/2) + 20 + pos + 0b10000000;
-            }
-            break;
-        default:
-            pos = pos;
+    switch (lcd->no_lines)
+    {
+    case 2:
+        pos = 64 * line + pos + 0b10000000;
+        break;
+    case 4:
+        if (line == 0 || line == 2)
+        {
+            pos = 64 * (line / 2) + pos + 0b10000000;
+        }
+        else
+        {
+            pos = 64 * ((line - 1) / 2) + 20 + pos + 0b10000000;
+        }
+        break;
+    default:
+        pos = pos;
     }
     uint_into_8bits(eight_bits, pos);
     send_full_byte(lcd, COMMAND, eight_bits);
 }
 
-void LCDdisplay_print(LCDdisplay* lcd, const char* str) {
+void LCDdisplay_print(LCDdisplay *lcd, const char *str)
+{
     uint32_t eight_bits[8];
     int i = 0;
-    while (str[i] != 0) {
+    printf("Printing to LCD: %s\n", str); // Debugging statement
+    while (str[i] != 0)
+    {
         uint_into_8bits(eight_bits, (uint32_t)(str[i]));
         send_full_byte(lcd, DATA, eight_bits);
         ++i;
     }
 }
 
-void LCDdisplay_print_wrapped(LCDdisplay* lcd, const char* str) {
+void LCDdisplay_print_wrapped(LCDdisplay *lcd, const char *str)
+{
     uint32_t eight_bits[8];
     int i = 0;
 
     LCDdisplay_goto_pos(lcd, 0, 0);
 
-    while (str[i] != 0) {
+    while (str[i] != 0)
+    {
         uint_into_8bits(eight_bits, (uint32_t)(str[i]));
         send_full_byte(lcd, DATA, eight_bits);
         ++i;
-        if (i % lcd->no_chars == 0) {
+        if (i % lcd->no_chars == 0)
+        {
             LCDdisplay_goto_pos(lcd, 0, i / lcd->no_chars);
         }
     }
 }
-
--- MCU2/WaterTank.c
+++ MCU2/WaterTank.c
@@ -9,22 +9,18 @@
 // Communication Drivers
 #include <Drivers/Communication/uart_communication.h>
 
-
-
 // Define the GPIO pin for the buzzer
 #define BUZZER_PIN 15
 
 // Define the GPIO pins for the LCD
-
 #define LCD_PIN_D4 6
 #define LCD_PIN_D5 7
 #define LCD_PIN_D6 8
 #define LCD_PIN_D7 9
-
-#define LCD_BL_PIN 10  // For backlight control (optional)
-
+#define LCD_BL_PIN 10 // For backlight control (optional)
 #define LCD_PIN_RS 11
-#define LCD_PIN_E  12
+#define LCD_PIN_E 12
+
 // Define the UART parameters
 #define UART_BAUD_RATE 9600
 #define UART_ID uart0
@@ -33,14 +29,13 @@
 
 LCDdisplay lcd;
 
-int main() {
-
+int main()
+{
     // Initialize actuators
     buzzer_init(BUZZER_PIN);
     int rc = pico_led_init();
     hard_assert(rc == PICO_OK);
 
- 
     stdio_usb_init(); // Initialize stdio for debugging
 
     // Initialize the UART communication
@@ -48,32 +43,39 @@ int main() {
 
     // Initialize the LCD display
     LCDdisplay_init_with_bl(&lcd,
-        LCD_PIN_D4,
-        LCD_PIN_D5,
-        LCD_PIN_D6,
-        LCD_PIN_D7,
-        LCD_PIN_RS,
-        LCD_PIN_E,
-        LCD_BL_PIN,
-        16, 2);  // 16x2 LCD
+                            LCD_PIN_D4,
+                            LCD_PIN_D5,
+                            LCD_PIN_D6,
+                            LCD_PIN_D7,
+                            LCD_PIN_RS,
+                            LCD_PIN_E,
+                            LCD_BL_PIN,
+                            16, 2); // 16x2 LCD
 
-    while (true) {
+    // Initialize the LCD display
+    LCDdisplay_init_display(&lcd);
+
+    while (true)
+    {
         pico_set_led(true);
 
-        printf("UART Initialized on Baud rate : %d\n",bandRate);  // Print message via stdio
+        printf("UART Initialized on Baud rate : %d\n", bandRate); // Print message via stdio
 
         // Clear the LCD and display a welcome message
         LCDdisplay_clear(&lcd);
         LCDdisplay_print(&lcd, "Waiting for UART");
+
         // receive a message over UART
-        char* received_msg = uart_receive_message();
-        if(received_msg != NULL) {
+        char *received_msg = uart_receive_message();
+        if (received_msg != NULL)
+        {
             printf("Received message: %s\n", received_msg);
 
             // Clear LCD and display appropriate messages
             LCDdisplay_clear(&lcd);
 
-            switch (received_msg[0]) {
+            switch (received_msg[0])
+            {
             case '1':
                 LCDdisplay_print(&lcd, "Water level: Low");
                 printf("Water level is low\n");
@@ -94,10 +96,9 @@ int main() {
                 printf("Invalid message\n");
                 buzzer_on();
                 break;
-        }
             }
+        }
 
-        
         sleep_ms(1000);
         pico_set_led(false);
     }
